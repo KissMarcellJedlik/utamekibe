e24347157825a2cac94df4bcd279af53e3a4f0fc
hutteraron3 <hutter.aron@students.jedlik.eu>
2023-10-16 13:54:13 +0200

    csabika

--- kod/__pycache__/csabika.py
+++ kod/__pycache__/csabika.py
@@ -1,1 +1,9 @@
-print
+print('Az aluljáró előtt meglátod egyik ismerősödet, Csabikát, aki az búcsúban lőtt JBL-én hallgatja a muzsikát. Mit teszel?')
+1 == print('Megpróbálod elkerülni.')
+2 == print('Odamész.')
+valasztas = int(input('Válassz merre akarod folytatni az utad.'))
+if valasztas == 1:
+    print('Lehúzod')
+    print('Kérsz tőle kaját')
+    print('Beszólsz neki, hogy hallgasson valami normális nótát')
+    print('Adsz neki egy százast')
