31f0d48e0ac55b35806e2a35132039ed99b28b97
Ikker Benjámin <ikker.benjamin@students.jedlik.eu>
2023-10-16 13:52:30 +0200

    értékek kijavítva

--- kod/__pycache__/kezdes.py
+++ kod/__pycache__/kezdes.py
@@ -1,19 +1,32 @@
-energia = 50
+energia = 100
 ehseg = 75
 penz = 500
 
 
-print("A főhősünk Ernesztó, megérkezett a hintód, leszálltál az autóbusz állomásnál, Győr pöcegödrébe (vidéki).")
-print("Éhes vagy, és nincs sok petákod (500Ft). Célod, hogy életben maradj és elzsalj a mekibe, ahol legyen elég jussod, hogy tudj magadnak vásárolni egy bejövős ajánlatot (1390Ft).")
-print("Próbálj meg sértetlen maradni, valamint ne kötözködj sokat a hobókkal!")
-print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
-print("Kezdő statok:")
-print("Pénz: 500Ft")
-print("Éhség: 75 %")
-print("Energia: 100%")
-print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+def kezdo_valasztas():
 
 
-print(f"Energia: {energia}")
-print(f"Éhség: {ehseg}")
-print(f"Pénz: {penz}")
+    print("A főhősünk Ernesztó, megérkezett a hintód, leszálltál az autóbusz állomásnál, Győr pöcegödrébe (vidéki).")
+    print("Éhes vagy, és nincs sok petákod (500Ft). Célod, hogy életben maradj és elzsalj a mekibe, ahol legyen elég jussod, hogy tudj magadnak vásárolni egy bejövős ajánlatot (1390Ft).")
+    print("Próbálj meg sértetlen maradni, valamint ne kötözködj sokat a hobókkal!")
+    print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    print("Kezdő statok:")
+    print("Pénz: 500Ft")
+    print("Éhség: 75 %")
+    print("Energia: 100%")
+    print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+
+    while energia > 0:
+        print("━━━━━━━━━━━━━━━━━━━━━━━")
+        print(f"Energia: {energia} %")
+        print(f"Éhség: {ehseg} %")
+        print(f"Pénz: {penz} Ft")
+        print("━━━━━━━━━━━━━━━━━━━━━━━")
+        break
+
+    print("Az orrodat megcsavarja az égett benzinből áradó füst, amely a buszok kipufogójából ered, a füled pedig brazilmagyarok ordibálásától cseng.")
+    print("Gondold meg mely útvonalon próbálsz szerencsét.")
+kezdo_valasztas()
+
+    
+    
