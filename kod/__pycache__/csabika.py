9d4a744cd41dcd6be472c638f011c4ad49bbaca5
hutteraron3 <hutter.aron@students.jedlik.eu>
2023-10-17 08:36:20 +0200

    csabika folytatás

--- kod/__pycache__/csabika.py
+++ kod/__pycache__/csabika.py
@@ -2,12 +2,21 @@ import kezdes
 
 print(kezdes.kezdo_valasztas)
 
-print('Az aluljáró előtt meglátod egyik ismerősödet, Csabikát, aki az búcsúban lőtt JBL-én hallgatja a muzsikát. Mit teszel?')
-1 == print('Megpróbálod elkerülni.')
-2 == print('Odamész.')
-valasztas = int(input('Válassz merre akarod folytatni az utad.'))
-if valasztas == 1:
-    print('Lehúzod')
-    print('Kérsz tőle kaját')
-    print('Beszólsz neki, hogy hallgasson valami normális nótát')
-    print('Adsz neki egy százast')
+def csabika():
+
+    print('Az aluljáró előtt meglátod egyik ismerősödet, Csabikát, aki az búcsúban lőtt JBL-én hallgatja a muzsikát. Mit teszel?')
+    
+    print("━━━━━━━━━━━━━━━━━━━━━━━")
+    1 == print('1 - Megpróbálod elkerülni.')
+    2 == print('2 - Odamész.')
+    print("━━━━━━━━━━━━━━━━━━━━━━━")
+    valasztas = int(input('Választás: '))
+    if valasztas == 1:
+        print('Odaköszönt neked, de nem foglalkoztál vele. Később ezt lehet megbánod')
+
+    if valasztas == 2:
+        print('Lehúzod')
+        print('Kérsz tőle kaját')
+        print('Beszólsz neki, hogy hallgasson valami normális nótát')
+        print('Adsz neki egy százast')
+csabika()
