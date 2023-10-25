e8510633ada19550b0a565b1c4d4efcb5ea80d39
hutteraron3 <hutter.aron@students.jedlik.eu>
2023-10-25 13:16:18 +0200

    kucci - reaadme

--- kod/kuccipafum.py
+++ kod/kuccipafum.py
@@ -1,3 +1,5 @@
+parfum = False
+from kezdes import penz
 print ('Sikeresen kiértél az  aluljáróból, átkeltél a fehér-fekete felfestésen. Ezután a Városháza mentén meglátsz egy hobót, aki kiszúrt téged, kezében egy "Kucci" parfümmel.')
 print ('"Szép napot honfitárs, szia uram, parfüm érdekel?"')
 print("━━━━━━━━━━━━━━━━━━━━━━━")
@@ -6,3 +8,15 @@ print("━━━━━━━━━━━━━━━━━━━━━━━")
 3 == print('3 - "Add ide a suskád, te hobó!"')
 4 == print('4 - "Érdekelne"')
 print("━━━━━━━━━━━━━━━━━━━━━━━")
+valasztas = int(input('Választás: '))
+    if valasztas == 1:
+        print('')
+        print('')
+    if valasztas == 2:
+        print()
+    if valasztas == 3:
+        print('')
+    if valasztas == 4:
+        print('')
+        parfum == True
+        penz - 500
